77bb55e12cc88fd555713fdacad21399ba21760f
Fraanlol <franco.catas@hotmail.com>
2022-02-06 22:57:59 -0300

    Settled events and 'dateset' command for retrieving date

--- commands/ping.js
+++ commands/ping.js
@@ -3,11 +3,18 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong!')
+		.addIntegerOption(date => 
+			date.setName('cumple')
+			.setDescription('Jojo')
+			.setRequired(true)
+		),
 	async execute(interaction) {
 		await interaction.reply('Pong!');
+		const integer = interaction.options.getInteger('cumple');
+		console.log(integer);
 		//INTERESTING, fetchs the mesage "Pong", usefull for adding reactions and stuff.
 		// const message = await interaction.fetchReply();
-		// console.log(message);
+        // console.log(message);
 	},
 };
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
 const fs = require('fs');
 
-// Command Handler 
+// Command Handler
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -13,19 +13,6 @@ for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
 }
-
-client.on('interactionCreate', async (interaction) => {
-    const command = client.commands.get(interaction.commandName);
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-
 
 //Event Handler
 
@@ -41,5 +28,3 @@ for (const file of eventFiles) {
 }
 
 client.login(process.env.bot_token);
-
-
